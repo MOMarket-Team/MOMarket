f4e921b54b74c2dd2bdb5845d003e59da373424e
Kabagambe Herbert <117737205+khabert@users.noreply.github.com>
2024-12-28 13:01:09 +0300

    Added spicescategory

--- Backend/index.js
+++ Backend/index.js
@@ -77,7 +77,7 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     cb(
       null,
-      $`{file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
     );
   },
 });
--- KCCA-Markets/src/App.jsx
+++ KCCA-Markets/src/App.jsx
@@ -12,6 +12,7 @@ import fruit_banner from './Components/Assets/Fruits_banner.jpg'
 import food_banner from './Components/Assets/Foods_banner.jpg'
 import veg_banner from './Components/Assets/vegetable_banner.jpg'
 import sauce_banner from './Components/Assets/Meat_banner.jpg'
+import spice_banner from './Components/Assets/Spices_banner.jpg'
 import Checkout from "./pages/Checkout.jsx";
 import SearchResults from "./pages/SearchResults.jsx";
 
@@ -26,6 +27,7 @@ function App() {
           <Route path='/Foods' element={<ProductsCategory banner={food_banner} category="Foods" />} />
           <Route path='/Vegetables' element={<ProductsCategory banner={veg_banner} category="Vegetables" />} />
           <Route path='/Sauce' element={<ProductsCategory banner={sauce_banner} category="Sauce" />} />
+          <Route path='/Spices' element={<ProductsCategory banner={spice_banner} category="Spices" />} />
           <Route path="/Product" element={<Product />}>
             <Route path=':productId' element={<Product />} />
           </Route>
