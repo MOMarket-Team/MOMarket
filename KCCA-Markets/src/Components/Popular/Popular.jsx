e7e1fd49db8fe7ebf8b836dd43ba21b49ddda236
isiagi <isiagigeofrey0@gmail.com>
2025-01-08 11:32:21 +0300

    chore: change url

--- KCCA-Markets/src/Components/Popular/Popular.jsx
+++ KCCA-Markets/src/Components/Popular/Popular.jsx
@@ -1,30 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import './Popular.css'
+import React, { useEffect, useState } from "react";
+import "./Popular.css";
 //import data_product from '../Assets/data'. nolonger needed coz of fetching from DB
-import Item from '../Item/Item'
+import Item from "../Item/Item";
 
 const Popular = () => {
-
   // all process of fetching from DB
-  const [popularProducts,setPopularProducts] = useState([]);
-  useEffect(()=>{
-    fetch('http://localhost:4000/popularinfruit')
-    .then((response)=>response.json())
-    .then((data)=>setPopularProducts(data));
-  },[])
+  const [popularProducts, setPopularProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://momarket.onrender.com/popularinfruit")
+      .then((response) => response.json())
+      .then((data) => setPopularProducts(data));
+  }, []);
   return (
-    <div className='popular'>
-        <h1>POPULAR FRUITS</h1>
-        <hr />
-        <div className="popular-item">
-            {popularProducts.map((item,i)=>{
-                return <Item key={i} id={item.id} 
-                name={item.name} image={item.image}
-                price={item.price}/>
-            })}
-        </div>
+    <div className="popular">
+      <h1>POPULAR FRUITS</h1>
+      <hr />
+      <div className="popular-item">
+        {popularProducts.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              price={item.price}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Popular
+export default Popular;
