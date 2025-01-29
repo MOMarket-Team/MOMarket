f7807166f22a3be09eb358fd047eea8483debf5c
Kabagambe Herbert <117737205+khabert@users.noreply.github.com>
2025-01-29 17:51:25 +0300

    styling

--- KCCA-Markets/src/Components/NewsLetter/NewsLetter.jsx
+++ KCCA-Markets/src/Components/NewsLetter/NewsLetter.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import './NewsLetter.css'
+import React from 'react';
+import './NewsLetter.css';
 
 const NewsLetter = () => {
   return (
     <div className='newsletter'>
-        <h1>Get Exclusive Offers On Your Email</h1>
-        <p>Subscribe to our newsletter and stay updated</p>
+        <h1>Fresh Deals, Straight to Your Inbox!</h1>
+        <p>Subscribe now to get exclusive discounts on fresh produce and market specials.</p>
         <div>
-            <input type="email" placeholder='Your Email Id' />
-            <button>Subscribe</button>
+            <input type="email" placeholder='Enter your email' />
+            <button>Subscribe Now</button>
         </div>
     </div>
-  )
+  );
 }
 
-export default NewsLetter
+export default NewsLetter;
