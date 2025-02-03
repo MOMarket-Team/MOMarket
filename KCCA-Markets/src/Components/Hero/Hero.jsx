7f26a07c546624a30cfdeff6c4c12facb713b616
Kabagambe Herbert <117737205+khabert@users.noreply.github.com>
2025-02-03 20:50:47 +0300

    image display

--- KCCA-Markets/src/Components/Hero/Hero.jsx
+++ KCCA-Markets/src/Components/Hero/Hero.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Hero.css";
 import hero_image from "../Assets/hero_image.png";
+import groce from "../Assets/groce.jpeg";
+import groce1 from "../Assets/groce1.jpg";
+import groce2 from "../Assets/groce2.jpeg";
+
+const images = [hero_image, groce, groce1, groce2];
 
 const Hero = ({ handleManualOrder }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  // Function to go to the next image
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  // Function to go to the previous image
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  // Auto-slide every 5 seconds
+  useEffect(() => {
+    const interval = setInterval(nextImage, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="hero">
       <div className="hero-left">
@@ -14,7 +39,9 @@ const Hero = ({ handleManualOrder }) => {
         </button>
       </div>
       <div className="hero-right">
-        <img src={hero_image} alt="Hero" />
+        <button className="arrow left" onClick={prevImage}>&#8592;</button>
+        <img src={images[currentImageIndex]} alt="Hero" />
+        <button className="arrow right" onClick={nextImage}>&#8594;</button>
       </div>
     </div>
   );
